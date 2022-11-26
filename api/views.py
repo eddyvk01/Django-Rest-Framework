ecac4aa0be3eced7a8ea8341cf96847c9f30c909
vineet <vineet123@gmail.com>
2022-11-26 10:13:57 +0530

    adding more views

--- api/views.py
+++ api/views.py
@@ -5,11 +5,22 @@ from .serializers import StudentSerializer
 from django.http import HttpResponse
 
 # Create your views here.
-
-def student_detail(request):
-    stu= Student.objects.get(id =  1) # sql query
+# query set - single student
+def student_detail(request, pk):
+    stu= Student.objects.get(id =  pk) # sql query
     # print(stu)
     serializer = StudentSerializer(stu)  # CONVERT IT INTO PYTHON NATIVE
+    # print(serializer)
+    # print(serializer.data)
+    json_data = JSONRenderer().render(serializer.data) # CONVERT INTO JSON DATA
+    # print(json_data)
+    return HttpResponse(json_data,content_type='application/json')
+
+#Query set  - all student data
+def student_list(request):
+    stu= Student.objects.all() # sql query
+    # print(stu)
+    serializer = StudentSerializer(stu, many=True)  # CONVERT IT INTO PYTHON NATIVE
     # print(serializer)
     # print(serializer.data)
     json_data = JSONRenderer().render(serializer.data) # CONVERT INTO JSON DATA
