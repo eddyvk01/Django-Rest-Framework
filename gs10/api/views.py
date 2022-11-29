ce7d3c3579dea62ada170063dceb9502e427ec6f
vineet <vineet123@gmail.com>
2022-11-29 11:04:08 +0530

    mixing Mixins groups

--- gs10/api/views.py
+++ gs10/api/views.py
@@ -4,37 +4,28 @@ from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin,UpdateModelMixin,DestroyModelMixin, RetrieveModelMixin
 
-class StudentList(GenericAPIView, ListModelMixin):
+#list and Create
+class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
     def get(self,request, *args, **kwargs):
         return self.list(request, *args , **kwargs)
 
-class StudentCreate(GenericAPIView, CreateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
     def post(self,request, *args, **kwargs):
         return self.create(request, *args , **kwargs)
 
-class StudentRetrive(GenericAPIView, RetrieveModelMixin):
+#Retrive ,Update, Delete - PK required
+
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin , DestroyModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
     def get(self,request, *args, **kwargs):
         return self.retrieve(request, *args , **kwargs)
 
-class StudentUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
     def put(self,request, *args, **kwargs):
         return self.update(request, *args , **kwargs)
-
-class StudentDelete(GenericAPIView, DestroyModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
 
     def delete(self,request, *args, **kwargs):
         return self.destroy(request, *args , **kwargs)
--- gs10/gs10/urls.py
+++ gs10/gs10/urls.py
@@ -19,9 +19,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/', views.StudentList.as_view()),
-    # path('studentapi/', views.StudentCreate.as_view()),
-    # path('studentapi/<int:pk>/', views.StudentRetrive.as_view()),
-    # path('studentapi/<int:pk>/', views.StudentUpdate.as_view()),
-    path('studentapi/<int:pk>/', views.StudentDelete.as_view()),
+    path('studentapi/', views.LCStudentAPI.as_view()),
+    path('studentapi/<int:pk>/', views.RUDStudentAPI.as_view()),
 ]
