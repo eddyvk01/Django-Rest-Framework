1b87412bd44c9902adbc4fd1ea2a09aa05e9eed5
vineet <vineet123@gmail.com>
2022-12-02 21:41:37 +0530

    using custom token generation

--- gs18/gs18/urls.py
+++ gs18/gs18/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
+from api.auth import CustomAuthToken
 
 #creating router object
 router = DefaultRouter()
@@ -14,6 +15,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
     path('auth/',include('rest_framework.urls',namespace='rest_framework')),
-    path('gettoken/', obtain_auth_token)
+    path('gettoken/', CustomAuthToken.as_view())
     
 ]
